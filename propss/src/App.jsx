494b1ca7742610ac0c30c6bface8f97d7af8d74b
zarri2 <168563126+zarri2@users.noreply.github.com>
2024-05-29 18:29:40 +0500

    update

---
+++ propss/src/App.jsx
@@ -0,0 +1,22 @@
+import Car from './assets/components/ProopsComponent'
+import './App.css'
+
+
+
+function App() {
+  let person = {
+    name : "Ammad",
+    age : 18,
+    gender : "Male",
+    skill : "programming"
+    
+  }
+ 
+  return (
+    <>
+      <Car/>
+    </>
+  )
+}
+
+export default App
---
+++ propss/src/assets/components/ProopsComponent.jsx
@@ -0,0 +1,14 @@
+function Car () {
+    return (
+        <>
+        <h1>Person Properties</h1>
+        <p>Gender : {personalbar.gender}</p>
+        <p>Height : {person.height}</p>
+        <p>Name : {person.name} </p>
+        <p>Age : {person.age}</p>
+        <p>Skill : {person.skill}</p>
+
+        </>
+    )
+}
+export default Car;
