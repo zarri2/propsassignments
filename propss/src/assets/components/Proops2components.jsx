d9323b8c374c029e39c8a6f10dfd987254230e01
zarri2 <168563126+zarri2@users.noreply.github.com>
2024-06-03 19:08:22 +0500

    update6/3/2024

--- propss/src/assets/components/Proops2components.jsx
+++ propss/src/assets/components/Proops2components.jsx
@@ -5,10 +5,7 @@ function Person3 (proops) {
 // dot method
     return (
         <>
-{/*         <h1>3rd Method</h1>
-            <p>student1 : {student1}</p>
-            <p>student2 : {student2}</p>
-            <p>student3 : {student3}</p> */}
+
             <Person4 {...proops}/>
         
         </>
